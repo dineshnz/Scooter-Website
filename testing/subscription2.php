85ff880eff54d0d0de7c957e7b41608be697fcc8
dineshnz <31061381+dineshnz@users.noreply.github.com>
2019-07-29 18:41:08 +1200

    membership plan added, feature restriction based on user role added. stripe payment added

---
+++ testing/subscription2.php
@@ -0,0 +1,84 @@
+
+    <style>
+     .card{
+         width: 300px;
+     }
+
+     .first{
+         background: lightblue;
+     }
+
+     .second{
+         background: yellow;
+     }
+     .card:hover{
+         -webkit-transform: scale(1.05);
+         -moz-transform: scale(1.05);
+         -ms-transform: scale(1.05);
+         -o-transform: scale(1.05);
+         transform:scale(1.05);
+
+         -webkit-transition: all .3s ease-in-out;
+         -moz-transition: all .3s ease-in-out;
+         -ms-transition: all .3s ease-in-out;
+         -o-transition: all .3s ease-in-out;
+         transition:all .3s ease-in-out;
+     }
+        .price{ font-size: 72px; text-align: center;}
+        .currency { 
+            font-size: 25px;
+            position: relative;
+            top: -35px;
+
+        }
+
+        .type{
+            text-align: center;
+        }
+
+        .list-group-item{
+            border: 0px;
+            padding: 5px;
+            text-align: center;
+        }
+    
+    </style>
+    <div class="container">
+        <div class="row">
+            
+        <div class="col-md-12 d-flex justify-content-center">
+           <div class="card">
+                <div class="card-header first">
+                   <h1 class="type">Basic User</h1>
+                </div>
+                <div class="card-header first">
+                <h1 class="price"><span class="currency">$</span>9</h1>
+                    <h6 class="type">per month</h6>
+                </div>
+                <div class="card-body">
+                    
+                    <ul class="list-group">
+                        <li class="list-group-item">Partial access</li>
+                        <li class="list-group-item">Search Scooters</li>
+                        <li class="list-group-item">Rent Scooters</li>
+                    </ul>
+                </div>
+                <div class="card-footer d-flex justify-content-center">
+                <form action="subscriptionProcess.php?sid=1" method="POST">
+                    
+                    <script
+                        src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+                        data-key=<?php echo $stripeDetails['publishableKey'] ?>
+                        data-amount="900"
+                        data-name=  "Basic User"
+                        data-description= "Membership"
+                        data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
+                        data-locale="auto"
+                        data-currency="nzd">
+                    </script>
+                </form>
+                </div>
+            </div>
+           </div>
+    </div>
+    </div>
---
+++ testing/subscriptionProcess.php
@@ -0,0 +1,84 @@
+<?php
+//premium plan id: plan_FWQBL2Tt3IJs2d
+  require_once 'config/stripeConfig.php';
+  require_once 'config/db.php';
+
+session_start();
+$passport = $_SESSION['passport'];
+
+
+$products = array(
+    "sids" => ["1","2"],
+    "1" => "plan_FWQAA7qsuAknOF",
+    "2" => "plan_FWQBL2Tt3IJs2d"
+);
+
+if(!isset($_GET['sid']) || !in_array($_GET['sid'], $products['sids']) || !isset($_POST['stripeToken']) || !isset($_POST['stripeEmail'])){
+    header('location: userProfile.php');
+    exit();
+}
+
+
+
+// Token is created using Checkout or Elements!
+// Get the payment token ID submitted by the form:
+$token = $_POST['stripeToken'];
+$email = $_POST['stripeEmail'];
+$productID =$_GET['sid'];
+
+
+
+$customer = \Stripe\Customer::create([
+    'email' =>$email,
+    'source' =>$token
+]);
+
+\Stripe\Subscription::create([
+    "customer" => $customer->id,
+    "items" => [
+      [
+        "plan" => $products[$productID],
+      ],
+    ]
+  ]);
+
+      
+  global $conn;
+  
+    $sql = "SELECT * FROM users where passport = ? LIMIT 1";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('s', $passport);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->num_rows;
+    $stmt->close();
+
+
+    if($row > 0){
+        $user = mysqli_fetch_assoc($result);
+        $update_query = "UPDATE users SET userLevel = '$productID' WHERE passport = '$passport'";
+
+        if (mysqli_query($conn, $update_query)){
+            //log user in
+
+                if($productID ==1){
+                    $subscription = "Basic Membership";
+                }
+                if($productID ==2){
+                    $subscription = "Prime Membership";
+                }
+                
+           
+                $_SESSION['userLevel'] = $productID;
+                $_SESSION['message'] = "$passport you have successfully upgraded your account to $subscription Account. ";
+                $_SESSION['type'] = 'alert-success';
+                header('location: successPayment.php');
+                exit(0);
+
+        }else{
+            echo "user not found";
+        }
+    }
+
+   
+?>
