b26fd1d77cc46bcce7c299f32e9aee10554c431b
dineshnz <31061381+dineshnz@users.noreply.github.com>
2019-09-14 11:37:11 +1200

    header bug on scooterdetail page fixed styling added for profile page styling added for update password page

--- testing/myPassword.php
+++ testing/myPassword.php
@@ -17,31 +17,76 @@ if (isset($_GET['logout'])) {
 <html>
 <head>
   <title>My Password</title>
-  <link rel="stylesheet" type="text/css" href="style.css">  
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- Sandstone Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
+	integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	
+	<link rel="stylesheet" href="css/style.css">
+	
   <script type="text/javascript" src="js/myProfileUpdate.js"></script>
   
 </head>
 <body>
+<?php include('includes/profileHeader.php');?>
+	<div class="ts-main-content">
+		<?php include('includes/leftbar.php');?>
+    <div class="content-wrapper" >
+    
+     <!-- header of the page -->
+     <section class="page-header profile_page">
+      <div class="content">
+        <section class=" profile_page">
+          <div class="container">
+            <div class="page-header_wrap">
+              <div class="page-heading">
+                <h1>Update Password</h1>
+              </div>
+            </div>
+          </div>
+          <!-- Dark Overlay-->
+          <div class="dark-overlay"></div>
+        </section>
+      </section>
    <?php 
-   include('includes/profileSideBar.php'); 
+  
    require_once 'controllers/authController.php';
     //retrieve id as it is set as primary key in the database
    $id=$_SESSION['id'];
    ?>
-   <div class="update_password">
-    <h1>Update Password</h1>
-    <form action="myprofile_updateProcess.php" method="post">
-        <input type="hidden" id="id" name="id" required="required" readonly value="<?php echo $id?>"><br>
-        <label>Current Password: </label><br>
-        <input type="password" id="currentPassword" name="currentPassword" required="required"><br>
-        <label>New Password: </label><br>
-        <input type="password" id="newPassword" name="newPassword" required="required"><br>
-        <label>Confirm New Password: </label><br>
-        <input type="password" id="confirmPassword" name="confirmPassword" required="required"><br>
-        <input type="button" value="Change Password" onclick="updatePassword()">
-        <p id="response"></p>
-    </form>
+
+    <div class="row" style="margin-left:100px; margin-right:20px; margin-top:10px;">
+      <div class="col-sm-8">
+      <div class="update_password">
+        <h1>Update Password</h1>
+        <form>
+          <input type="hidden" id="id" name="id" required="required" readonly value="<?php echo $id?>">
+          <div class="form-group">
+              <label>Current Password:<span class="text-danger">*</span> </label>
+              <input type="password" id="currentPassword" class="form-control" name="currentPassword" required="required">
+          </div>
+          <div class="form-group">
+            <label>New Password:<span class="text-danger">*</span> </label>
+            <input type="password" id="newPassword" class="form-control" name="newPassword" required="required">
+          </div>
+          <div class="form-group">
+            <label>Confirm New Password:<span class="text-danger">*</span> </label>
+            <input type="password" id="confirmPassword" class="form-control" name="confirmPassword" required="required">
+          </div>
+          <div class="form-group">
+              <input type="button" value="Change Password" class="btn btn-primary form-control" onclick="updatePassword()">
+          </div>
+          <p id="response"></p>
+     </form>
+    </div>
+  </div>
+      <div class="col-sm-4">
+        <?php  include('includes/profileSideBar.php');  ?>
+      </div>
 </div>
+    </div></div>
+
+ 
 <!-- a form to retrieve all the user data from the database, user can change it and press the update button to update the data in the database-->
 </body>
 </html>
--- testing/myProfile.php
+++ testing/myProfile.php
@@ -15,14 +15,49 @@ if (isset($_GET['logout'])) {
 <html>
 <head>
   <title>My Profile</title>
-  <link rel="stylesheet" type="text/css" href="css/style1.css">  
+ 
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- Sandstone Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
+	integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	
+	<link rel="stylesheet" href="css/style.css">
+	
   <script type="text/javascript" src="js/myProfileUpdate.js"></script>
+  <style>
+    #length{
+      width: 500px;
+    }
+
+  </style>
 
 </head>
 <body>
+<?php include('includes/profileHeader.php');?>
+	<div class="ts-main-content">
+		<?php include('includes/leftbar.php');?>
+    <div class="content-wrapper" >
+    
+     <!-- header of the page -->
+     <section class="page-header profile_page">
+      <div class="content">
+        <section class=" profile_page">
+          <div class="container">
+            <div class="page-header_wrap">
+              <div class="page-heading">
+                <h1>Update Profile</h1>
+              </div>
+            </div>
+          </div>
+          <!-- Dark Overlay-->
+          <div class="dark-overlay"></div>
+        </section>
+      </section>
   <?php
-  include('includes/profileSideBar.php');
+ 
   require_once 'controllers/authController.php';
+
+  
 
   //Receive all the data based on what user has entered to login instead of searching in the database
   $id=$_SESSION['id'];
@@ -32,26 +67,94 @@ if (isset($_GET['logout'])) {
   $passport=$_SESSION['passport'];
   $driverLicense=$_SESSION['license'];
   ?>
-  <br>
-
-  <div class="personal_information">
+ 
+  <div class="row"  style="margin-left:100px; margin-right:20px; margin-top:10px;">
+    <div class="col-sm-8">
+    <div class="personal_information">
     <h1>General Information</h1>
-    <form action="" method="post">
-      <input type="hidden" id="id" name="id" required="required" readonly value="<?php echo $id?>"><br>
-      <label>Full Name:</label><br>
-      <input type="text" id="full_name" name="full_name" required="required" value="<?php echo $fullname?>"><br>
-      <label>Email Address: </label><br>
-      <input type="text" id="email_address" name="email_address" required="required" value="<?php echo $email?>"><br>
-      <label>Address: </label><br>
-      <input type="text" id="address" name="address" required="required" value="<?php echo $address?>"><br>
-      <label>Passport Number: <label><br>
-        <input type="text" id="passport_number" name="passport_number" required="required" value="<?php echo $passport?>"><br>
-        <label>Driver License: </label><br>
-        <input type="text" id="driver_license" name="driver_license" required="required" value="<?php echo $driverLicense?>"><br>
-        <input type="button" value="Update" onClick="updateData()">
+    <form >
+      <div class="form-group">
+         <input type="hidden" id="id" name="id" required="required" readonly value="<?php echo $id?>">
+      </div>
+      <div class="form-group" id="length">
+          <label class="control-label">Full Name:</label><br>
+          <input type="text" id="full_name" name="full_name" class="form-control"
+           required="required" value="<?php echo $fullname?>">
+      </div>
+
+      <div class="form-group" id="length">
+          <label class="control-label">Email Address: </label>
+          <input type="text" id="email_address" name="email_address" class="form-control"
+          required="required" value="<?php echo $email?>">
+      </div>
+      <div class="form-group" id="length">
+          <label class="control-label">Address: </label>
+          <input type="text" id="address" name="address" class="form-control"
+          required="required" value="<?php echo $address?>" >
+      </div>
+      <div class="form-group" id="length">
+          <label class="control-label">Passport Number: </label>
+          <input type="text" id="passport_number" name="passport_number" class="form-control"
+          required="required" value="<?php echo $passport?>" disabled 
+          title="Please send request to admin to update your passport number">
+      </div>
+      <div class="form-group" id="length">
+          <label class="form-group">Driver License: </label>
+          <input type="text" id="driver_license" name="driver_license" class="form-control"
+           required="required" value="<?php echo $driverLicense?>" disabled
+           title="Please send request to admin to update your Driver License number">
+      </div>
+      <div class="form-group" id="length">
+         <input type="button" class="btn btn-primary form-control" value="Update" onClick="updateData()">
+      </div>
+       
       </form>
       <p id="response"></p>
     </div>
+    </div>
+    
+
+    <!-- keeping menus on right -->
+    <div class="col-sm-4">
+        <?php  include('includes/profileSideBar.php'); ?>
+        <hr>
+        <strong><p>Send Request to admin to update passport and license</p></strong>
+        <form >
+          <div class="form-group">
+              <label class="control-label">First Name: </label>
+              <input type="text" name="fname" class="form-control" required="required" placeholder="First Name">
+          </div>
+           <div class="form-group">
+             <label class="control-label">Email:</label>
+             <input type="email" name="email" class="form-control" required="required" placeholder="Email">
+           </div>
+           <div class="form-group">
+             <label class="control-label">Subject:</label>
+             <input type="text" name="subject" class="form-control" required="required" placeholder="Subject">
+           </div>
+           <div class="form-group">
+              <label class="control-label">Message: </label>
+              <textarea name="message" id="message" class="form-control" rows="3" placeholder="Message"></textarea>
+           </div>
+          <div class="form-group">
+              <input type="submit" class="btb btn-primary" value="Submit">
+              <input type="reset" class="btb btn-danger" value="Reset">
+          </div> 
+           
+            
+  </form>
+    </div>
+  </div>
+
+
+
+  
+ 
     <!-- a form to retrieve all the user data from the database, user can change it and press the update button to update the data in the database-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+ 
   </body>
   </html>
